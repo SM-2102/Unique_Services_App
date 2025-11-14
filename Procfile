f726bddcb38b1c51a852bcea792cac151a368a2e
SM-2102 <sukanya.manna.2002@gmail.com>
2025-11-15 04:22:31 +0530

    railway

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: uvicorn backend.src:app --host 0.0.0.0 --port $PORT
--- api/index.py
+++
@@ -1,9 +0,0 @@
-import sys, os
-from serverless_fastapi import Handler
-
-# add backend folder to path
-sys.path.append(os.path.join(os.path.dirname(__file__), ".."))
-
-from backend.src import app
-
-handler = Handler(app)
