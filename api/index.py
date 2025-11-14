c3361ac085809e2464a0e064651ce361aae1c2c4
SM-2102 <sukanya.manna.2002@gmail.com>
2025-11-15 02:59:10 +0530

    vercel-v2

--- api/index.py
+++ api/index.py
@@ -1,4 +1,9 @@
+import sys, os
 from serverless_fastapi import Handler
+
+# add backend folder to path
+sys.path.append(os.path.join(os.path.dirname(__file__), ".."))
+
 from backend.src import app
 
 handler = Handler(app)
