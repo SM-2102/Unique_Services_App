e4537bc4899675302592e480a53591bd970b0c7f
SM-2102 <sukanya.manna.2002@gmail.com>
2025-12-08 20:13:35 +0530

    backup

---
+++ backend/backup/backup.bat
@@ -0,0 +1,4 @@
+@echo off
+cd /d "D:\DAD_OFFICE\Unique_Services_App\backend\src\backup"
+"D:\DAD_OFFICE\Unique_Services_App\backend\.venv\Scripts\python.exe" backup.py
+pause
--- backend/src/backup/backup.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-cd /d "D:\Unique_Services_App\backend\src\backup"
-"D:\Unique_Services_App\backend\.venv\Scripts\python.exe" backup.py
-pause
