5baabcb69e00c239339661829a7c644420518153
SM-2102 <sukanya.manna.2002@gmail.com>
2026-01-14 09:25:38 +0530

    jwt

--- backend/src/auth/utils.py
+++ backend/src/auth/utils.py
@@ -1,5 +1,5 @@
 import uuid
-from datetime import datetime, timedelta
+from datetime import datetime, timedelta, timezone
 
 import jwt
 from passlib.context import CryptContext
@@ -9,7 +9,7 @@ from exceptions import InvalidToken
 
 password_context = CryptContext(schemes=["bcrypt"])
 
-ACCESS_TOKEN_EXPIRY = 60
+ACCESS_TOKEN_EXPIRY = timedelta(hours=3)
 
 
 def generate_hash_password(password: str) -> str:
@@ -25,26 +25,26 @@ def create_user_token(user_data: dict, expiry: timedelta = None, refresh: bool =
     payload = {}
 
     payload["user"] = user_data
-    payload["exp"] = datetime.now() + (
-        expiry if expiry is not None else timedelta(seconds=ACCESS_TOKEN_EXPIRY)
-    )
+    payload["exp"] = datetime.now(timezone.utc) + (expiry or ACCESS_TOKEN_EXPIRY)
     payload["refresh"] = refresh
 
     token = jwt.encode(
-        payload=payload, key=Config.JWT_SECRET_KEY, algorithm=Config.JWT_ALGORITHM
+        payload=payload,
+        key=Config.JWT_SECRET_KEY,
+        algorithm=Config.JWT_ALGORITHM,
     )
-
     return token
 
 
 def decode_user_token(token: str) -> dict:
     try:
         token_data = jwt.decode(
-            jwt=token, key=Config.JWT_SECRET_KEY, algorithms=[Config.JWT_ALGORITHM]
+            jwt=token,
+            key=Config.JWT_SECRET_KEY,
+            algorithms=[Config.JWT_ALGORITHM],
         )
         return token_data
     except jwt.ExpiredSignatureError:
-        # Explicitly raise InvalidToken for expired tokens
         raise InvalidToken()
     except jwt.PyJWTError:
-        return None
+        raise InvalidToken()
